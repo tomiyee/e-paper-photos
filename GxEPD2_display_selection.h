e725cdfc7752a1a319457954f4c9efe0b6847d79
Tommy Seng Heng <tommy.and.heng@gmail.com>
2024-04-06 15:39:31 -0400

    refactor: removed irrelevant code for current hardware

--- GxEPD2_display_selection.h
+++ GxEPD2_display_selection.h
@@ -16,5 +16,5 @@
 // Add new topics in https://forum.arduino.cc/c/using-arduino/displays/23 for new questions and issues
 
 #if defined(ESP32)
-GxEPD2_BW<GxEPD2_420, GxEPD2_420::HEIGHT> display(GxEPD2_420(/*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4)); // GDEW042T2 400x300, UC8176 (IL0398)
+GxEPD2_BW<GxEPD2_420, GxEPD2_420::HEIGHT> display(GxEPD2_420(/*CS=5*/ SS, /*DC=*/17, /*RST=*/16, /*BUSY=*/4)); // GDEW042T2 400x300, UC8176 (IL0398)
 #endif
